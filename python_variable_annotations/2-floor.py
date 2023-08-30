cb313a6c6f24bd9b05a6e443fc1196ef0d57cb00
Ghelder <Jhelder@outlook.com>
2023-08-29 23:29:21 -0500

    feat: add function floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A module that contains a function that
+takes a float as input and returns"""
+
+
+def floor(n: float) -> float:
+    """A function that takes a float as input and
+      returns the floor value of the input.
+
+    Args:
+        n (float): The input number
+
+    Returns:
+        float: The floor value of the input number
+    """
+    return n
