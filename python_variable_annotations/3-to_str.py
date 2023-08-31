e6b9506b6931f8c6854eeef52d218c7355c39b0c
Ghelder <Jhelder@outlook.com>
2023-08-31 01:42:34 -0500

    feat: add function convert

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This module contains a function to convert a decimal
+number to a text string"""
+
+
+def to_str(n: float) -> str:
+    """Converts a float to a string.
+
+    Args:
+        n (float): The number to be converted to a string.
+
+    Returns:
+        str: The string representation of the number.
+    """
+    return str(n)
