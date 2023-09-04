1a2c8f8634d069f47998bbeacec97262b37a67bc
Ghelder <Jhelder@outlook.com>
2023-09-03 23:43:23 -0500

    add: function async generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""this module contains a function async generator"""
+from asyncio import sleep
+from random import uniform
+
+
+async def async_generator():
+    """
+    async generator that yields a random number
+    between 0 and 10 after waiting for 1 second.
+
+    Yields:
+        int: A random number between 0 and 10.
+    """
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
